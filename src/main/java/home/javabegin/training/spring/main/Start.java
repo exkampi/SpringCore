d4edcd2b89862858509ff0133c1fca9091feb271
WizardAdeptus <wizard.adeptus@ya.ru>
2017-03-09 10:16:49 +0300

    Autowiring

--- src/main/java/home/javabegin/training/spring/main/Start.java
+++ src/main/java/home/javabegin/training/spring/main/Start.java
@@ -11,22 +11,22 @@ public class Start {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
 
-		T1000Pool t1000Pool = (T1000Pool) context.getBean("t1000GoldenPool");
-		t1000Pool.beginShow();
+//		T1000Pool t1000Pool = (T1000Pool) context.getBean("t1000GoldenPool");
+//		t1000Pool.beginShow();
 
-//		RobotConveyor t1000Conveyor;
-//		t1000Conveyor = (RobotConveyor) context.getBean("t1000Conveyor");
-//
-//		Robot terminator1 = t1000Conveyor.createRobot();
-//		Robot terminator2 = t1000Conveyor.createRobot();
-//		Robot terminator3 = t1000Conveyor.createRobot();
-//
-//		System.out.println("terminator1 " + terminator1);
-//		System.out.println("terminator2 " + terminator2);
-//		System.out.println("terminator3 " + terminator3);
-//
-//		terminator1.action();
-//		terminator2.action();
-//		terminator3.action();
+		RobotConveyor t1000Conveyor;
+		t1000Conveyor = (RobotConveyor) context.getBean("t1000Conveyor");
+
+		Robot terminator1 = t1000Conveyor.createRobot();
+		Robot terminator2 = t1000Conveyor.createRobot();
+		Robot terminator3 = t1000Conveyor.createRobot();
+
+		System.out.println("terminator1 " + terminator1);
+		System.out.println("terminator2 " + terminator2);
+		System.out.println("terminator3 " + terminator3);
+
+		terminator1.action();
+		terminator2.action();
+		terminator3.action();
 	}
 }
