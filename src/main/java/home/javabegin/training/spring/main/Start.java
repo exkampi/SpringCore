c5861885ffcdedaf83fc673b624fb414d7bfefb3
WizardAdeptus <wizard.adeptus@ya.ru>
2017-03-31 23:02:28 +0300

    README

--- src/main/java/home/javabegin/training/spring/main/Start.java
+++ src/main/java/home/javabegin/training/spring/main/Start.java
@@ -1,5 +1,6 @@
 package home.javabegin.training.spring.main;
 
+import home.javabegin.training.spring.impls.robot.ModelT800;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,9 +9,19 @@ public class Start {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("all_context.xml");
 
+
 		System.out.println("TERMINATOR #1: " + context.getBean("modelT800"));
 		System.out.println("TERMINATOR #2: " + context.getBean("modelT800"));
 		System.out.println("TERMINATOR #3: " + context.getBean("model1"));
 		System.out.println("TERMINATOR #4: " + context.getBean("model1"));
+
+		((ModelT800)context.getBean("model1")).action();
+
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.err.println("ЭТОТ КОД ВЫПОЛНЯЕТСЯ ПРИ ЗАВЕРШЕНИИ РАБОТЫ ПРОГРАММЫ.");
+			}
+		}));
 	}
 }
